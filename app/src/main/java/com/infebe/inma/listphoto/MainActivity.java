005fb6563d76de5b99e2bc7a76c6c81a45402843
Inma <inmaferrer@gmail.com>
2017-11-29 14:36:14 +0100

    Created dynamically PhotoListFragment in MainActivity

--- app/src/main/java/com/infebe/inma/listphoto/MainActivity.java
+++ app/src/main/java/com/infebe/inma/listphoto/MainActivity.java
@@ -10,9 +10,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Check that the activity is using the layout version with
+        //the fragment_container FrameLayout
+        //When this exists, we will be in phone display
+        if(findViewById(R.id.fragment_container)!=null){
 
-        //When we are in phone display, add a new ListPhotoFragment dynamically
-        PhotosListFragment mPhotosFragment = new PhotosListFragment();
+            //If we are being restored from a previous state,
+            //then we don't need to do anything and should return or else
+            //we could end up with overlapping fragments
+            if(savedInstanceState != null){
+                return;
+            }
+
+            //Create a new Fragment to be placed in the activity layout
+            PhotosListFragment mPhotosFragment = new PhotosListFragment();
+
+            //Add fragment to the  'fragment_container' FrameLayout. We are dynamically inserting the fragment
+            getSupportFragmentManager().beginTransaction().add(R.id.fragment_container, mPhotosFragment).commit();
+        }
+
+
 
 
     }
